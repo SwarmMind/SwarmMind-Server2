a452dbc614dffa27834d572faccef43b6c7c308b
Leon <leon@matthes.biz>
2018-06-05 14:05:01 +0200

    some bugfixes

--- source/utilities/CommandBuilder.ts
+++ source/utilities/CommandBuilder.ts
@@ -2,6 +2,8 @@ import AttackCommand from './AttackCommand';
 import Command from './Command';
 import MoveCommand from './MoveCommand';
 
+import { Point, Vector } from 'flatten-js';
+
 
 export default class CommandBuilder {
     static commandTypeList = new Map([
@@ -9,8 +11,9 @@ export default class CommandBuilder {
         ['move', MoveCommand],
     ]);
 
-    static build(type, ...constructorArguments) {
-        return new CommandBuilder.commandTypeList[type](...constructorArguments);
+    static build(type, ID, direction) {
+        console.log(direction);
+        return new (CommandBuilder.commandTypeList.get(type))(ID, new Vector(new Point(0, 0), new Point(direction.x, direction.y)));
     }
 
     static get types(): string[] {
