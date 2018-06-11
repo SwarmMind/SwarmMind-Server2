86ea5e44852611dfae1aeb9151fbe1532bd7f2a0
Leon <leon@matthes.biz>
2018-06-11 14:45:02 +0200

    Fixed type of DieCommands not set

--- source/utilities/DieCommand.ts
+++ source/utilities/DieCommand.ts
@@ -2,6 +2,13 @@ import Command from './Command';
 import Game from '../GameModule/Game';
 
 export default class DieCommand extends Command{
+
+    constructor(mapObjectID: number) {
+        super(mapObjectID);
+
+        this._type = 'die';
+    }
+
     protected executionFunction(game: Game){
         game.killMapObject(this.mapObjectID);
     }
