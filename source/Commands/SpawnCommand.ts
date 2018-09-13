3e22605596386eb80fd503820d6ed66a29472d1a
Tom Braun <brauntom2@t-online.de>
2018-09-13 15:58:56 +0200

    some improvements for spawn commands

--- source/Commands/SpawnCommand.ts
+++ source/Commands/SpawnCommand.ts
@@ -1,12 +1,20 @@
 import Command from './Command';
 import Game from '../GameModule/Game';
+import MapObject from '../GameModule/MapObject';
 
 export default class SpawnCommand extends Command {
-    constructor(mapObjectID: number) {
-        super(mapObjectID);
+    constructor(private mapObject: MapObject) {
+        super(mapObject.ID);
+
+        this.mapObject = mapObject;
 
         this._type = 'spawn';
     }
 
     protected executionFunction(game: Game) {}
+
+    public serialize() {
+        return Object.assign(super.serialize(),
+            {isPlayer: this.mapObject.isPlayer(), position: this.mapObject.position});
+    }
 }
